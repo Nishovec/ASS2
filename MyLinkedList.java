c30ff96b5696113c218845dbbdaed2dc189130cb
Nishovec <shaymardanov_E@inbox.ru>
2023-04-18 21:54:09 +0600

    A

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,6 +11,7 @@ public class MyLinkedList<E> implements MyList<E> {
             this.prev = null;
         }
     }
+
     private Node head;
     private Node tail;
     private int size;
@@ -21,5 +22,71 @@ public class MyLinkedList<E> implements MyList<E> {
         size = 0;
     }
 
+    public void add(E element) {
+        Node newNode = new Node(element);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
 
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.prev;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current.element;
+
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+        if (current.next == null) {
+            tail = current.prev;
+
+        } else {
+            current.next.prev = current.prev;
+
+        }
+        size--;
+        return current.element;
+
+    }
 }
