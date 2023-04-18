67483be0659e2dd3bc0b5ba2da5c8fbbc18c6d01
Nishovec <shaymardanov_E@inbox.ru>
2023-04-18 09:45:16 +0600

    A

--- MyArrayList.java
+++ MyArrayList.java
@@ -2,4 +2,19 @@ public class MyArrayList<E> implements MyList<E> {
     private Object[] elements;
     private int size;
 
+    public class MyArrayList() {
+        elements = new Object[10];
+        size =0;
+    }
+
+    public void add(E element) {
+        if (size== elements.length) {
+            Object[] newElements = new Object[2 * elements.length];
+            for (int i=0; i < elements.length; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+        elements[size] = element;
+    }
 }
